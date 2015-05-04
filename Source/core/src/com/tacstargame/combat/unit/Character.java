6d72cc163453194d130442f39601c5f221294853
DCSTUDIOS <domenikirrgang@web.de>
2015-05-04 16:13:43 +0200

    Moved files

--- Source/core/src/com/tacstargame/combat/unit/Character.java
+++
@@ -1,9 +0,0 @@
-package com.tacstargame.combat.unit;
-
-import com.tacstargame.combat.gear.GearSet;
-import com.tacstargame.combat.unit.stats.Stats;
-
-public interface Character extends Unit {
-	Stats getGearStats();
-	GearSet getGearSet();
-}
